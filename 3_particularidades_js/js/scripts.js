b701a1326cefeb6ce947ead9513c15c9e6660723
Claudio128 <cllaudio128@gmail.com>
2023-01-16 00:50:15 -0300

    Particularidades do JavaScript

---
+++ 3_particularidades_js/js/scripts.js
@@ -0,0 +1,16 @@
+console.log("Primeiro");
+console.log("Último");
+console.log("Mais um consolo.log");
+
+// Tipagem fraca
+var a = 1;
+a = 'teste';
+console.log(a);
+
+// Comentários com uma linha
+
+/*
+Comentários
+Com múltiplas
+Linhas
+*/
