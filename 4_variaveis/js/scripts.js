6f26cd98baf5662af409a201a6030daf3ac80eb2
Claudio128 <cllaudio128@gmail.com>
2023-01-16 01:10:13 -0300

    Variáveis JavaScript

---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,33 @@
+var teste = 1;
+console.log(teste);
+
+teste = "Claudio";
+console.log(teste);
+
+var nome = "Andrade"
+console.log(nome)
+
+// Apenas Cifrão e Underline podem ser colocados antes de uma variável
+var $nome = "Cifrão";
+var _nome = "Underline";
+console.log($nome);
+console.log(_nome);
+
+var nome5 = "Número";
+console.log(nome5);
+
+var meuPrimeiroNome = "Claudio";
+console.log(meuPrimeiroNome);
+
+var meusobrenome = "Andrade";
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+console.log(meuNome);
+meuNome = "Claudio";
+console.log(meuNome);
