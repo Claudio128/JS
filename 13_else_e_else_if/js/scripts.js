be8a289a4e74038c26747d357868ce1db4212038
Claudio128 <cllaudio128@gmail.com>
2023-01-18 00:03:02 -0300

    Else e Else if

---
+++ 13_else_e_else_if/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Claudio";
+
+if (nome == "Pedro") {
+    console.log("O nome dele é Pedro!");
+} else if (nome == "Xavier") {
+    console.log("O nome é Xavier");
+} else if (nome == "Claudio") {
+    console.log("O nome é Claudio!");
+} else {
+    console.log("Ele possui outro nome!");
+}
+
+var idade = 19;
+
+if (idade > 20){
+    console.log("Ele pode entrar na festa!");
+} else if (idade >= 18){
+    console.log("Ele só pode entrar com a autorização!");
+}
+
+if (nome == "Claudio") {
+    console.log("Teste");
+} else {
+    console.log("Testando");
+}
