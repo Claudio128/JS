737e5c367c1a825ae5ee176a28ca55ed809ee797
Claudio128 <cllaudio128@gmail.com>
2023-01-17 22:00:12 -0300

    Arrays

---
+++ 10_array/js/scripts.js
@@ -0,0 +1,18 @@
+var arr = [5, "Claudio", true, {nome: "Lenda", idade: 20}]; // obj => {}
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr2[0]);
+
+arr[4] = [10];
+
+arr[0] = "Mago";
+
+console.log(arr);
+
+console.log(typeof arr);
