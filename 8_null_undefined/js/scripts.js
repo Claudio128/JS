2df0b721a183e2917011141a66b05f49dfb5eb88
Claudio128 <cllaudio128@gmail.com>
2023-01-17 21:35:58 -0300

    Tipos de dados: Null e Undefined

---
+++ 8_null_undefined/js/scripts.js
@@ -0,0 +1,15 @@
+// Hosting - içamento
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome = "Andrade";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Claudio";
+
+console.log(nome);
+
+var numero = 5;
