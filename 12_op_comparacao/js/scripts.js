4e665cf567f56a85139169f4af28152821226131
Claudio128 <cllaudio128@gmail.com>
2023-01-17 23:36:35 -0300

    Operadores de comparação

---
+++ 12_op_comparacao/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 18;
+var possuiCarro = 1;
+
+if (idade >= 18) {
+    console.log("Você está apto para fazer a carteira!");
+}
+
+if (idade <= 17) {
+    console.log("Espere mais um pouco para fazer a carteira!");
+}
+
+if (possuiCarro) {
+    console.log("Você possui um carro!");
+}
+
+var nome = "Pedro";
+
+if (nome == "Claudio") {
+    console.log("O seu nome é Claudio!");
+}
+
+if (nome != "Claudio") {
+    console.log("O seu nome não é Claudio!");
+}
+
+if (20 > 10) {
+    console.log("Passou");
+}
+
+if (100 < 20) {
+    console.log("Passou 2");
+}
