457c65d295c470dc5ef7d0901172f6993f176009
Claudio128 <cllaudio128@gmail.com>
2023-01-21 19:17:49 -0300

    Estrutura de repetição: While

---
+++ 18_while/js/scripts.js
@@ -0,0 +1,28 @@
+var x = 0;
+
+while(x < 5) {
+    console.log("Testendo repetição" + x);
+
+    // Incrementador
+    x++;
+}
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+with(y <= 3){
+    console.log(arr[y]);
+
+    // Incrementador
+    y++;
+}
+
+var palavra = "Claudio";
+var i = 0;
+
+while (i <= 6) {
+    console.log(palavra[i]);
+
+    // Incrementador
+    i++;
+}
